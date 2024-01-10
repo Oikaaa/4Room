d4ff91f5aae6ed5d58ea3c39a5584dbdf374843d
Oikaaa <jimmythang2017@gmail.com>
2024-01-10 14:27:09 +0700

    News update

--- News.js
+++ News.js
@@ -1,15 +1,15 @@
 async function getData(){
         try{
-            const topApi = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=cbb75045607748118c709034a876df83');
+            const topApi = await fetch('https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=fb89f17e213903567d42a53003554ff0');
             const topData = await topApi.json();
-            console.log(topData);
-
-            const top2Api = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=cbb75045607748118c709034a876df83');
-            const top2Data = await top2Api.json();
-            console.log(top2Data);
+            console.log(topData.url);
 
             topData.articles.forEach(function(item, index){
-                console.log(item)
+                console.log(item.url)
+                const a = document.createElement('a')
+                a.classList.add('post')
+                a.href = item.url
+
                 const post = document.createElement('div')
                 post.classList.add('post')
     
@@ -30,47 +30,19 @@ async function getData(){
 
                 const img = document.createElement('img')
                 img.classList.add('Img')
-                img.src = item?.urlToImage
+                img.src = item?.image
     
-                document.getElementById('postZone').appendChild(post)
+                document.getElementById('postZone').appendChild(a)
+                a.appendChild(post)
                 post.appendChild(hello)
                 hello.appendChild(header)
                 hello.appendChild(description)
                 hello.appendChild(content)
                 hello.appendChild(img)
-            })
-            top2Data.articles.forEach(function(item, index){
-                console.log(item)
-                const post = document.createElement('div')
-                post.classList.add('post')
-    
-                const hello = document.createElement('div')
-                hello.classList.add('hello')
-    
-                const header = document.createElement('h1')
-                header.classList.add('postHeader')
-                header.innerText = item.title
-    
-                const content = document.createElement('div')
-                content.classList.add('textBox' , 'con')
-                description.classList.add('con')
-                content.innerText = item.content
 
-                const description = document.createElement('div')
-                description.classList.add('textBox' , 'des')
-                description.innerText = item.description
-
-                const img = document.createElement('img')
-                img.classList.add('shopImg')
-                img.src = item?.urlToImage
-    
-                document.getElementById('postZone').appendChild(post)
-                post.appendChild(hello)
-                hello.appendChild(header)
-                hello.appendChild(description)
-                hello.appendChild(content)
-                hello.appendChild(img)
+                
             })
+            
         }
         catch(e){
             console.log(e)
@@ -82,5 +54,3 @@ async function getData(){
     setTimeout(function(){
         loading.style.display = 'none'
     },5000)
-
-    
